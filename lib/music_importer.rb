62839bed1e8789c7827ce188b58958fda5c70366
Mike Shahindoust <gobias101@gmail.com>
2020-09-04 22:15:13 +0000

    Automatically backed up by Learn

---
+++ lib/music_importer.rb
@@ -0,0 +1,11 @@
+class MusicImporter
+  attr_reader :path
+  
+  def initialize(path)
+    @path = path
+  end
+  
+  def files
+    @files ||= Dir.glob("#{path}/*.mp3").collect{ |i| i.gsub("#{path}/", "")}
+  end
+end
